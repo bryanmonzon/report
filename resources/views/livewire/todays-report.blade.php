8606199431193b9cb883a9b5bb500b2e03e98f9a
Bryan Monzon <monzonb@gmail.com>
2022-12-14 21:40:31 -0800

    wip

--- resources/views/livewire/todays-report.blade.php
+++ resources/views/livewire/todays-report.blade.php
@@ -1,5 +1,5 @@
 <x-wire-elements-pro::tailwind.slide-over>
-    <x-slot name="title">Report for {{now()->toFormattedDateString()}} </x-slot>
+    <x-slot name="title">Report for {{now()->timezone(auth()->user()->timezone)->toFormattedDateString()}} </x-slot>
 
     <div class="text-sm text-gray-800">
         Hi there! The demo application you are exploring contains various dummy actions and it seems you just triggered one!
